5c309a5f8f2076ae785e0518637f730e8c9f9541
jozs83 <samca83@gmail.com>
2024-04-09 22:56:57 -0400

    para ivangay

--- src/app/admin/pages/form-att/form-att.component.ts
+++ src/app/admin/pages/form-att/form-att.component.ts
@@ -1,66 +1,111 @@
-import { Component, OnInit , ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { OfficerService } from '../../services/officer.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
-
 @Component({
   selector: 'app-form-att',
   templateUrl: './form-att.component.html',
   styleUrls: ['./form-att.component.css'],
-  
 })
 export class FormAttComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   dataSource = new MatTableDataSource<any>([]);
   FormAtt: FormGroup;
-  text: string = ''
-  displayedColumns = ['nombre', 'estado', 'options']
+  text: string = '';
+  displayedColumns = ['fecha', 'hora', 'options'];
+
+  intervaloID: any;
+  ejecutando: boolean = false;
 
   constructor(
     private fb: FormBuilder,
-    private funcionariosService: OfficerService,
-  ) {
-  }
+    private funcionariosService: OfficerService
+  ) {}
 
   ngOnInit() {
     this.FormAtt = this.fb.group({
-      horas: ['24', Validators.required]
+      horas: ['24', Validators.required],
+      dateRange: ['', Validators.required] 
     });
-    this.Get()
+    this.Get();
   }
-
 
   Get() {
     if (this.text !== '') {
-      this.funcionariosService.search(this.text).subscribe(data => {
-        this.dataSource = new MatTableDataSource(data.officers)
+      this.funcionariosService.search(this.text).subscribe((data) => {
+        this.dataSource = new MatTableDataSource(data.officers);
         this.dataSource.paginator = this.paginator;
-      })
-    }
-    else {
-      this.funcionariosService.get().subscribe(data => {
-        this.dataSource = new MatTableDataSource(data.officers)
+      });
+    } else {
+      this.funcionariosService.get().subscribe((data) => {
+        this.dataSource = new MatTableDataSource(data.officers);
         this.dataSource.paginator = this.paginator;
-      })
+      });
     }
   }
 
   applyFilter(event: Event) {
     this.text = (event.target as HTMLInputElement).value;
-    this.Get()
+    this.Get();
   }
 
   cancelSearch() {
-    this.text = ''
-    this.Get()
-  }
-  Descargar(fecha: any) {
-  }
-  Eliminar(fecha: any) {
+    this.text = '';
+    this.Get();
   }
 
-  Guardar(){}
+  DescargarJson(fecha: any) {
+    console.log(fecha)
+    
+  }
+
+  DescargarExcel(fecha: any) {
+    console.log(fecha)
+  }
+
+  toggleComenzar() {
+    const horasValue = this.FormAtt.get('horas')?.value;
+  
+    if (this.ejecutando) {
+      clearInterval(this.intervaloID);
+      this.ejecutando = false;
+      this.FormAtt.get('horas')?.enable();
+    } else {
+      if (horasValue !== null && horasValue !== undefined) {
+        const intervaloMinutos = horasValue * 60 * 1000; 
+        this.intervaloID = setInterval(() => {
+          const now = new Date();
+          const fecha = this.formatDate(now);
+          const fechaHora = now.toISOString(); 
+          const registro = {
+            fecha: fecha,
+            hora: now.toLocaleTimeString(), 
+            fecha_hora: fechaHora 
+          };
+         
+          this.dataSource.data.push(registro); 
+          this.dataSource._updateChangeSubscription(); 
+          console.log('Nuevo registro:', registro);
+        }, intervaloMinutos);
+  
+        this.ejecutando = true;
+        this.FormAtt.get('horas')?.disable();
+      } else {
+        console.log('El valor de horas es null o undefined');
+      }
+    }
+  }
+
+  formatDate(date: Date): string {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+    const formattedDay = day < 10 ? `0${day}` : `${day}`;
+    return `${formattedMonth}-${formattedDay}-${year}`;
+  }
 
 }
---
+++ src/app/admin/services/att.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttService {
+
+  constructor(private http: HttpClient) {}
+
+  get() {
+    return this.http.get<{ levels: any[], length: number }>(`${base_url}/levels`).pipe(
+      map(resp => {
+        return { levels: resp.levels, length: resp.length }
+      })
+    )
+  }
+}
+
